81c2d2a62d91151aacdc98cfb770b24109b52f8d
sator4iiik <sator4iiik@gmail.com>
2021-06-28 18:38:24 +0300

    Sprint 01 rdy

---
+++ FRONTEND/JS/jsv2/jsv2_01/app_l/js/script.js
@@ -0,0 +1,28 @@
+console.log("Hello");
+console.log("world!");
+console.log("333");
+console.log(334);
+
+console.log("Hello" + " " + "World!");
+console.info('BEBEBE');
+
+// alert("WTF?");
+// alert("Why you there?");
+
+
+document.getElementById("out").innerHTML = "Yo"
+document.getElementById("out").innerHTML = "Y2019"
+document.getElementById("out").innerHTML = "<b>2020<b>"
+
+document.querySelector('.header').innerHTML = 5;
+document.querySelector('#one').innerHTML = 777; // # есть
+document.qetElementById('one').innerHTML = 888;
+
+// let a;
+
+let b = document.querySelector('#one'); // получение параграфа внутрь
+let c;
+c = document.querySelector('.header');
+
+a.innerHTML = 9999; //
+c.innerHTML = 0909; //
