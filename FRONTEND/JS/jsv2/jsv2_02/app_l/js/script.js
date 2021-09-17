f91541b0ead9ee320e76eb2e2801c4c2b4d59d45
sator4iiik <sator4iiik@gmail.com>
2021-09-17 22:03:22 +0300

    initial commit

---
+++ FRONTEND/JS/jsv2/jsv2_02/app_l/js/script.js
@@ -0,0 +1,22 @@
+//////////////////////Выполняю по умолчанию/////////////////////
+// по загрузке страницы
+let a = 6;
+let b = 'Hello';
+
+console.log(a);
+
+let inputIn = document.querySelector('.input-in'); // input
+let button  = document.querySelector('button');     // button
+let div     = document.querySelector('.out');
+
+///////////////////////Событие//////////////////////////////////
+
+button.onclick = function (){
+    // кнопка будет нажата
+    console.log('работает!');
+    // console.log( inputIn.value ); // value - то что введено в input
+    let b = +inputIn.value;          // + перевести в число
+    console.log(b + 10);             // воспринимает как строку
+    div.innerHTML = b;
+    inputIn.value = '';              // очистить строку после себя
+}
