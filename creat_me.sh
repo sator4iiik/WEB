dfc82ab8cda74c8c702ef2af686315d9d006ad4a
sator4iiik <sator4iiik@gmail.com>
2021-10-10 16:10:12 +0300

    initial commit

---
+++ creat_me.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir t00 |
+
+mkdir ./t00/css           |
+mkdir ./t00/js            |
+touch ./t00/index.html    |
+touch ./t00/css/style.css |
+touch ./t00/js/app.js     | echo "done"
