fb9a139f94df7d95c3849af2880205c694638892
sator4iiik <sator4iiik@gmail.com>
2021-10-13 16:34:17 +0300

    finish aim

--- FRONTEND/JS/randtask/board/js/appV1.js
+++ FRONTEND/JS/randtask/board/js/appV1.js
@@ -29,6 +29,7 @@ function removeColor(element) {
 }
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length)
-    return colors[index]
+    return colors[Math.floor(Math.random() * colors.length)]
+    // const index = Math.floor(Math.random() * colors.length)
+    // return colors[index]
 }
