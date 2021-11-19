b191514cbe2d81e31f1edabe7057c25142f54ef4
sator4iiik <sator4iiik@gmail.com>
2021-11-19 05:57:06 +0200

    fff

--- FRONTEND/JS/randtask/00Random/wu/d00/app.js
+++ FRONTEND/JS/randtask/00Random/wu/d00/app.js
@@ -19,11 +19,58 @@ let stadium = Boolean(true);
 
 let   h = 40;
 let   w = 70;
-const d = "cm";
+const d = "см";
 
-let res = h * w + d;
+let areaRectangle = h * w;
 
-console.log(res)
+console.log(areaRectangle + d);
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  */ // Задание 3:
+
+let      time = 2;
+
+speedOfFirst  = 95;
+speedOfSecond = 114;
+
+let distance  = (speedOfFirst + speedOfSecond) * time;
+
+console.log(distance);
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ */ // Задание 4:
+
+const randomNumber = Math.floor(Math.random() * 100);
+const infoT04 = "if ↓";
+
+console.log(infoT04);
+
+if (randomNumber < 20) {
+    console.log("randomNumber меньше 20");
+} else if (randomNumber > 50) {
+    console.log("randomNumber больше 50");
+} else {
+    console.log("randomNumber больше 20, и меньше 50");
+}
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ */ // Задание 5:
+
+const randomNumber2 = Math.floor(Math.random() * 100);
+
+const infoT05 = "switch ↓";
+
+console.log(infoT05);
+switch (true) {
+    case (randomNumber2 < 20):
+        console.log("randomNumber меньше 20");
+        break;
+
+    case (randomNumber2 > 50):
+        console.log("randomNumber больше 50");
+        break;
+
+    default:
+        console.log("randomNumber больше 20, и меньше 50");
+
+}
