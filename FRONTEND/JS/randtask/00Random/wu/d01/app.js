e29c38874bf533cb49786cd99b1b6373908d1ed2
sator4iiik <sator4iiik@gmail.com>
2021-11-23 07:07:04 +0200

    initial commit

--- FRONTEND/JS/randtask/00Random/wu/d01/app.js
+++ FRONTEND/JS/randtask/00Random/wu/d01/app.js
@@ -48,9 +48,9 @@ const array = [
 ];
 
 let result = (array[2] + ' ' + array[6] + ' ' + array[11] + ' ' + array[0]
-+ ' ' + array[7] + ' ' + array[3] + ' ' + array[4] + ' ' +
-array[8] + ' ' + array[9] + ' ' + array[12] + ' ' + array[1]
-+ ' ' + array[10] + ' ' + array[5])
+                + ' ' + array[7] + ' ' + array[3] + ' ' + array[4] + ' ' +
+                array[8] + ' ' + array[9] + ' ' + array[12] + ' ' + array[1]
+                + ' ' + array[10] + ' ' + array[5])
 
 console.log(result)
 
