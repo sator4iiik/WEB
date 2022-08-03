3e48032c42ed0f929a88130c50d7a6ab265f2993
ooreznikov <oleksii.reznikov@beter.live>
2022-08-03 15:42:04 +0300

    add-to-card

---
+++ FRONTEND/REACT/add-to-card/src/data.js
@@ -0,0 +1,31 @@
+import img1 from './img/img1.jpeg';
+import img2 from './img/img2.jpeg';
+import img3 from './img/img3.jpeg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Juisy Grapes',
+            desc: '',
+            price: 48,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Red Watermelon',
+            desc: '',
+            price: 56,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Juicy Orange',
+            desc: '',
+            price: 31,
+        }
+    ],
+};
+
+export default data;
